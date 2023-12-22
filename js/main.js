34c29d17aacdbd5ecd67423fccb8fad8b573daa5
Marwa Mahmoud <mm3764453@gmail.com>
2023-12-23 00:14:27 +0200

    _

--- js/main.js
+++ js/main.js
@@ -5,12 +5,15 @@
 var SiteNameInput = document.getElementById("SiteNameInput");
 var SiteUrlInput = document.getElementById("SiteUrlInput");
 var urlList = [];
+var modal = document.getElementById("exampleModal");
+modal.removeAttribute("id");
 
 if (localStorage.getItem("urls") != null) {
   urlList = JSON.parse(localStorage.getItem("urls"));
   display();
 }
 function addUrl() {
+if (validateName() && ValidateUrl()) {
   var urls = {
     name: SiteNameInput.value,
     url: SiteUrlInput.value,
@@ -19,6 +22,11 @@ function addUrl() {
   localStorage.setItem("urls", JSON.stringify(urlList));
   display();
   clear();
+} else {
+  var modal = document.querySelector(".modal");
+  console.log(modal);
+  modal.setAttribute("id", "exampleModal");
+}
 }
 function display() {
   var cartoona = "";
@@ -47,5 +55,31 @@ function clear() {
 
 // validation
 function validateName(){
-
+var regexName = /^[a-z]{3,15}$/
+var text= SiteNameInput.value;
+if(regexName.test(text)){
+    SiteNameInput.classList.add("is-valid");
+      SiteNameInput.classList.remove("is-invalid");
+      return true;
 }
+else{
+       SiteNameInput.classList.add("is-invalid");
+       SiteNameInput.classList.remove("is-valid"); 
+       return false;
+}
+}
+
+function ValidateUrl(){
+var urlRegex = /^[a-z]{3,15}.com$/;
+var text = SiteUrlInput.value;
+if (urlRegex.test(text)) {
+  SiteUrlInput.classList.add("is-valid");
+  SiteUrlInput.classList.remove("is-invalid");
+  return true;
+} else {
+  SiteUrlInput.classList.add("is-invalid");
+  SiteUrlInput.classList.remove("is-valid");
+  return false;
+}
+}
+
